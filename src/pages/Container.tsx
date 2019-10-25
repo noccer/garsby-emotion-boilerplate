faa5e146eafd411f3036048fa54ad230d1ae6816
Niall O'Callaghan <noccer@gmail.com>
2019-10-25 21:39:23 +1100

    Added global styles to container

--- src/pages/Container.tsx
+++ src/pages/Container.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import { lightTheme } from '../utils/theme';
+import { Global, css } from '@emotion/core';
 
 interface LayoutProps {}
 
 export const Container: React.FunctionComponent<LayoutProps> = (props) => {
     return (
         <ThemeProvider theme={lightTheme}>
-            <section className="sectiony">{props.children}</section>
+            <Global
+                styles={css`
+                    body {
+                        font-family: sans-serif;
+                        // todo: include CSS reset here instead
+                    }
+                `}
+            />
+            {props.children}
         </ThemeProvider>
     );
 };
