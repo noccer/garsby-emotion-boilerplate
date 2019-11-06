4b7d9c1eaf3a67b352e6f9fe773efd270563160a
Niall O'Callaghan <noccer@gmail.com>
2019-11-06 23:59:57 +1100

    Refactoring button component.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,18 +16,22 @@ interface ButtonProps {
 
 const Button: React.FunctionComponent<ButtonProps> = ({ children, label, onClick }) => {
     const theme = useTheme<NiallTheme>();
+    const { colors, buttons, lineHeights } = theme;
+    const { body } = lineHeights;
+    const { secondary, primary: color } = colors;
+    const { bg: backgroundColor } = buttons.primary;
 
     const cssStyles: InterpolationWithTheme<NiallTheme> = css({
         ':focus': {
             outline: 'none',
             borderRadius: '100px',
-            boxShadow: `0px 0px 3px ${theme.colors.secondary}`,
+            boxShadow: `0px 0px 3px ${secondary}`,
         },
-        color: theme.colors.primary,
-        backgroundColor: theme.buttons.primary.bg,
+        color,
+        backgroundColor,
         borderRadius: '100px',
         border: 'none',
-        padding: `${theme.lineHeights.body / 3}rem ${theme.lineHeights.body / 2}rem`,
+        padding: `${body / 3}rem ${body / 2}rem`,
         // outline: 'green', // TODO: use a context and detect 'userIsTabbing'
         // https://medium.com/hackernoon/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2
     });
